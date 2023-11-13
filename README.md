65af8d90d4ab62df8d5d7c0de999eb472f0170d2
Dmytro <moiseenkodmitriy@i.ua>
2023-11-13 14:40:27 +0200

    lesson17

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# react-course
+# react-course (lesson 17)
+
+- [Redux Toolkit](https://redux-toolkit.js.org/)
+- [React Query](https://tanstack.com/query/v3/)
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { App } from './components/App';
+import { ThemeProvider } from 'styled-components';
+import { store } from './redux/store';
+import { theme } from './constants/theme';
+import 'normalize.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);

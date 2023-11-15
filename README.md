c6e2fd0559cd761dc2ea191f38d67354833f0bd0
Dmytro <moiseenkodmitriy@i.ua>
2023-11-15 20:49:51 +0200

    lesson19

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-course
+# react-course (lesson 19)
+
+- Пишем асинхронный поток действий вручную
+- Рефакторим импорты с Re-export
+- Алиасы и абсолютные импорты
+- createAsyncThunk
+- createSlice
---
+++ src/views/BooksView.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+import { Link, useRouteMatch, useLocation } from 'react-router-dom';
+import slugify from 'slugify';
+import { useSelector, useDispatch } from 'react-redux';
+import { booksOperations, booksSelectors } from '../redux/books';
+import PageHeading from '../components/PageHeading/PageHeading';
+
+const makeSlug = string => slugify(string, { lower: true });
+
+export default function BooksView() {
+  const location = useLocation();
+  const { url } = useRouteMatch();
+  const dispatch = useDispatch();
+  const books = useSelector(booksSelectors.getBooks);
+
+  // useEffect(() => dispatch(booksOperations.fetchBooks()), [dispatch]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      try {
+        dispatch(booksOperations.fetchBooks());
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  return (
+    <>
+      <PageHeading text="Книги" />
+
+      {books.length > 0 && (
+        <ul>
+          {books.map(book => (
+            <li key={book.id}>
+              <Link
+                to={{
+                  pathname: `${url}/${makeSlug(`${book.title} ${book.id}`)}`,
+                  state: {
+                    from: {
+                      location,
+                      label: 'Назад к всем книгами',
+                    },
+                  },
+                }}
+              >
+                {book.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}

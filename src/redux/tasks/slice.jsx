67874d0fece0391bdcf59a5e2d7bd6b71183a9fc
Dmytro <moiseenkodmitriy@i.ua>
2023-11-18 03:20:41 +0200

    deploy

--- src/redux/tasks/slice.jsx
+++ src/redux/tasks/slice.jsx
@@ -18,34 +18,36 @@ const tasksSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  extraReducers: {
-    [fetchTasks.pending]: handlePending,
-    [addTask.pending]: handlePending,
-    [deleteTask.pending]: handlePending,
-    [fetchTasks.rejected]: handleRejected,
-    [addTask.rejected]: handleRejected,
-    [deleteTask.rejected]: handleRejected,
-    [fetchTasks.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [addTask.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [deleteTask.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(task => task.id === action.payload.id);
-      state.items.splice(index, 1);
-    },
-    [logOut.fulfilled](state) {
-      state.items = [];
-      state.error = null;
-      state.isLoading = false;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchTasks.pending, handlePending)
+      .addCase(addTask.pending, handlePending)
+      .addCase(deleteTask.pending, handlePending)
+      .addCase(fetchTasks.rejected, handleRejected)
+      .addCase(addTask.rejected, handleRejected)
+      .addCase(deleteTask.rejected, handleRejected)
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(task => task.id === action.payload.id);
+        state.items.splice(index, 1);
+      })
+      .addCase(logOut.fulfilled, state => {
+        state.items = [];
+        state.error = null;
+        state.isLoading = false;
+      });
   },
 });
 
